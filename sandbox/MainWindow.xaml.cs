7732e0677e0b4358c8ad543a6f101229bd19ccff
koloniz <hubkat99@gmail.com>
2017-01-25 08:50:23 +0100

    Přidal sem si zapisování do souboru

--- sandbox/MainWindow.xaml.cs
+++ sandbox/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,38 @@ namespace sandbox
         public MainWindow()
         {
             InitializeComponent();
+
+            string[] lines = {"First line", "Second line", "Third line"};
+            System.IO.File.WriteAllLines(@"C:\Users\Tom\Desktop\Škola\VS\sandbox\WriteLines.txt", lines);
+
+            string text = "A class is the most powerful data type in C#. Like structures, " +
+                       "a class defines the data and behavior of the data type. ";
+            System.IO.File.WriteAllText(@"C:\Users\Tom\Desktop\Škola\VS\sandbox\WriteText.txt", text);
+
+            List<Item> Itemy =  new List<Item>();
+            int[,] statboostage = new int[,] { { 1,3 } };
+
+            for(int a = 0; a < 5;a++) {
+                Item item_ = new Item("Pussydestroyer",1,statboostage);
+                Itemy.Add(item_);
+            }
+            string serializationFile = @"C:\Users\Tom\Desktop\Škola\VS\sandbox\itemy.bin";
+
+            //serialize
+            using (Stream stream = File.Open(serializationFile, FileMode.Create))
+            {
+                var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+
+                bformatter.Serialize(stream, Itemy);
+            }
+
+            //deserialize
+            using (Stream stream = File.Open(serializationFile, FileMode.Open))
+            {
+                var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+
+                List<Item>  Itemy_ = (List<Item>)bformatter.Deserialize(stream);
+            }
         }
     }
 }
---
+++ sandbox/item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sandbox
+{
+    class Item
+    {
+        public string name;
+        public int item_type;
+        public int[,] statboost;
+
+        public Item(string Name, int Item_type, int[,] Statboost) {
+            name = Name;
+            item_type = Item_type;
+            statboost = Statboost;
+        }
+    }
+}
