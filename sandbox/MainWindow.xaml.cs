d245b27edb908641699915a63ca04cd37b9bf94f
koloniz <hubkat99@gmail.com>
2017-01-25 11:07:47 +0100

    som na to došel

--- sandbox/MainWindow.xaml.cs
+++ sandbox/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -21,6 +22,32 @@ namespace sandbox
     /// </summary>
     public partial class MainWindow : Window
     {
+        List <Item> Itemky = new List<Item>();
+        int control;
+        bool answer;
+        public void LoadJsonFile() {
+            using (StreamReader r = new StreamReader(@"C:\Users\Tom\Desktop\Škola\VS\sandbox\test.json")){
+                string json = r.ReadToEnd();
+                Itemky = JsonConvert.DeserializeObject<List<Item>>(json);
+            }
+        }
+        public bool IsEnchanted(int predmet) {
+            int[,] statboostager = Itemky[predmet].statboost;
+            foreach(int r in statboostager) {
+                control++;
+            }
+            switch (control)
+            {
+                case 2:
+                    answer = false;
+                    break;
+                case 4:
+                    answer = true;
+                    break;
+            }
+            return answer;
+            
+        }
         public MainWindow()
         {
             InitializeComponent();
@@ -33,16 +60,21 @@ namespace sandbox
             System.IO.File.WriteAllText(@"C:\Users\Tom\Desktop\Škola\VS\sandbox\WriteText.txt", text);
 
             List<Item> Itemy =  new List<Item>();
-            int[,] statboostage = new int[,] { { 1,3 } };
-
+            int[,] statboostage = new int[,] { { 1,3 } , { 2,4 } };
+            int[,] statboostage_ = new int[,] { { 5,1 } };
             for(int a = 0; a < 5;a++) {
                 Item item_ = new Item("Pussydestroyer",1,statboostage);
                 Itemy.Add(item_);
             }
+            Item item_2 = new Item("Pussydestroyerka",2,statboostage_);
+            Itemy.Add(item_2);
             string json = JsonConvert.SerializeObject(Itemy.ToArray());
 
             //write string to file
-            System.IO.File.WriteAllText(@"C:\Users\Tom\Desktop\Škola\VS\sandbox\path.txt", json);
+            System.IO.File.WriteAllText(@"C:\Users\Tom\Desktop\Škola\VS\sandbox\test.json", json);
+
+            LoadJsonFile();
+            text1.Text = IsEnchanted(1).ToString();
         }
     }
 }
