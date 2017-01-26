ba97c2517c55c8248e91784fb02baaf37c5d35e9
koloniz <hubkat99@gmail.com>
2017-01-26 08:07:06 +0100

    nevim

--- sandbox/MainWindow.xaml.cs
+++ sandbox/MainWindow.xaml.cs
@@ -22,13 +22,60 @@ namespace sandbox
     /// </summary>
     public partial class MainWindow : Window
     {
+        string line;
         List <Item> Itemky = new List<Item>();
+        List <Item> Merchant_Itemky = new List<Item>();
+        string[,] name_of_item = new string[,] { 
+            {
+                "Železný meč","Železná přilba","Železný hrudní plát","Železné kalhoty","Železné boty"
+            },
+            {
+                "Zlatý meč","Zlatá přilba","Zlatý hrudní plát","Zlaté kalhoty","Zlaté boty"
+            },
+            {
+                "Diamantový meč","Diamantová přilba","Diamantový hrudní plát","Diamantové kalhoty","Diamantové boty"
+            }
+        };
+        string[,] name_of_enchant = new string[,]{
+            {
+                "zdraví","síly","obrana"
+            }
+        };
+        static Random rnd0 = new Random();
+        int random_item_type;
+        int random_item_level;
+        //int is_enchanted;
+        int[,] statboostager;
+		//int random_item_type = rnd0.Next(1,5);
         int control;
         bool answer;
         public void LoadJsonFile() {
             using (StreamReader r = new StreamReader(@"C:\Users\Tom\Desktop\Škola\VS\sandbox\test.json")){
                 string json = r.ReadToEnd();
                 Itemky = JsonConvert.DeserializeObject<List<Item>>(json);
+            }
+        }
+        public void GenerateMerchantInventory(){
+            Item item;
+            for(int a=1;a<=15;a++) {
+                random_item_type = rnd0.Next(0,5);
+                random_item_level = rnd0.Next(0,3);
+                if(random_item_type == 1) {
+                    statboostager = new int[,] { { 1,(1+random_item_level) } };
+                }
+                else {
+                    statboostager = new int[,] { { 3,(1+random_item_level)*2,5 } };
+                }
+                //is_enchanted = rnd0.Next(1,2);
+                item = new Item(name_of_item[random_item_level,random_item_type],random_item_type,statboostager);
+                bool alreadyIn = Merchant_Itemky.Any(derp => derp == item);
+
+                if(alreadyIn == true) {
+                    a = a - 1;
+                }
+                else {
+                    Merchant_Itemky.Add(item);
+                }  
             }
         }
         public bool IsEnchanted(int predmet) {
@@ -74,7 +121,12 @@ namespace sandbox
             System.IO.File.WriteAllText(@"C:\Users\Tom\Desktop\Škola\VS\sandbox\test.json", json);
 
             LoadJsonFile();
-            text1.Text = IsEnchanted(1).ToString();
+
+            GenerateMerchantInventory();
+            for(int a=0;a<=14;a++){
+                line += Merchant_Itemky[a].name.ToString()+Environment.NewLine;
+            }
+            text1.Text = line;
         }
     }
 }
